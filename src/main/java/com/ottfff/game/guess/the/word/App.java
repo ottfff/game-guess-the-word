5f1fd58542f209d050b026db71d20d6e08fdcca5
Eduard <ottfff@mail.ru>
2025-06-11 20:41:03 +0300

    hello

--- src/main/java/com/ottfff/game/guess/the/word/App.java
+++ src/main/java/com/ottfff/game/guess/the/word/App.java
@@ -1,7 +1,19 @@
 package com.ottfff.game.guess.the.word;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws IOException {
+        var bytes = Files.readString(Paths.get("src/main/resources/some-book.txt"));
+        log.info("File length: {}", bytes.length());
     }
 }
